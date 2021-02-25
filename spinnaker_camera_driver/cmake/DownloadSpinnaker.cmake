5cdcacee14ed13e2cf4cca54a77da48a557fdd44
Stephan Wirth <1481786+stwirth@users.noreply.github.com>
2021-02-25 10:28:22 +0100

    ExternalProject_Add failure

--- spinnaker_camera_driver/cmake/DownloadSpinnaker.cmake
+++ spinnaker_camera_driver/cmake/DownloadSpinnaker.cmake
@@ -1,21 +1,50 @@
-function(download_spinnaker FLIR_LIB_VAR FLIR_INCLUDE_DIR_VAR)
+
+function(download_spinnaker SPINNAKER_SDK_DIR)
   if(NOT UNIX)
-    message(FATAL_ERROR "Downloading libSpinnaker for non-linux systems not supported")
+    message(FATAL_ERROR "Downloading libSpinnaker for non-linux systems not supported.")
   endif()
 
+  # First we find out which library version we need (OS version, architecture, SDK version)
   execute_process(
-    COMMAND lsb_release -cs
-    OUTPUT_VARIABLE OS_CODE_NAME
+    COMMAND lsb_release -rs
+    OUTPUT_VARIABLE OS_VERSION
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   include(cmake/TargetArch.cmake)
-  target_architecture(FLIR_ARCH)
-  set(FLIR_DIR ${CMAKE_CURRENT_BINARY_DIR}/usr/lib)
-  set(DOWNLOAD_SCRIPT "${PROJECT_SOURCE_DIR}/cmake/download_spinnaker")
+  target_architecture(TARGET_ARCH)
 
-  message(STATUS "Running download_spinnaker script with arguments: ${FLIR_ARCH} ${FLIR_DIR} ${OS_CODE_NAME}")
-  execute_process(
-    COMMAND ${DOWNLOAD_SCRIPT} ${FLIR_ARCH} "${FLIR_DIR}" ${OS_CODE_NAME})
-  set(${FLIR_LIB_VAR} "${FLIR_DIR}/libSpinnaker.so" PARENT_SCOPE)
-  set(${FLIR_INCLUDE_DIR_VAR} "${CMAKE_CURRENT_BINARY_DIR}/usr/include/spinnaker" PARENT_SCOPE)
+  if("${OS_VERSION}" STREQUAL "16.04")
+    set(SDK_VERSION "1.27.0.48")
+  else()
+    set(SDK_VERSION "2.2.0.48")
+  endif()
+
+  if("${TARGET_ARCH}" STREQUAL "i386")
+    set(SDK_ARCH i386)
+  elseif("${TARGET_ARCH}" STREQUAL "x86_64")
+    set(SDK_ARCH "amd64")
+  elseif("${TARGET_ARCH}" STREQUAL "armv7")
+    set(SDK_ARCH "armhf")
+  elseif("${TARGET_ARCH}" STREQUAL "armv8")
+    set(SDK_ARCH "arm64")
+  else()
+    message(FATAL_ERROR "Downloading libSpinnaker for architecture ${TARGET_ARCH} not supported.")
+  endif()
+
+  set(SDK_URL "https://packages.clearpathrobotics.com/stable/flir/Spinnaker/Ubuntu${OS_VERSION}/spinnaker-${SDK_VERSION}-Ubuntu${OS_VERSION}-${SDK_ARCH}-pkg.tar.gz")
+  file(DOWNLOAD SDK_URL ${CMAKE_BINARY_DIR}/spinnaker-sdk.tar.gz)
+  file(ARCHIVE_EXTRACT INPUT spinnaker-sdk.tar.gz VERBOSE)
+
+  # Download and extract tarball with debians
+  #  FetchContent_Declare(spinnaker-sdk
+  #  PREFIX spinnaker-sdk
+  #  BUILD_IN_SOURCE 1
+  #  CONFIGURE_COMMAND ""
+  #  BUILD_COMMAND dpkg --extract libspinnaker-${SDK_VERSION}_${SDK_ARCH}.deb . && dpkg --extract libspinnaker-${SDK_VERSION}_${SDK_ARCH}-dev.deb .
+  #  INSTALL_COMMAND ""
+  #)
+
+  # Set output variable
+  #FetchContent_Get_Property(spinnaker-sdk SOURCE_DIR)
+  set(${SPINNAKER_SDK_DIR} "${SOURCE_DIR}" PARENT_SCOPE)
 endfunction()
--- spinnaker_camera_driver/cmake/modules/FindSpinnaker.cmake
+++ spinnaker_camera_driver/cmake/modules/FindSpinnaker.cmake
@@ -1,22 +1,20 @@
 unset(Spinnaker_FOUND)
-unset(Spinnaker_INCLUDE_DIRS)
+unset(Spinnaker_INCLUDE_DIR)
 unset(Spinnaker_LIBRARIES)
 
-find_path(Spinnaker_INCLUDE_DIRS NAMES
-  Spinnaker.h
-  PATHS
-  /usr/include/spinnaker/
-  /usr/local/include/spinnaker/
-  /opt/spinnaker/include/
-)
+# Search in Spinnaker_SDK_ROOT if it is set.
+if(Spinnaker_SDK_ROOT)
+  message("Spinnaker_SDK_ROOT defined as: ${Spinnaker_SDK_ROOT}")
+  set(_SPINNAKER_SEARCH_ROOT ${Spinnaker_SDK_ROOT}/usr ${Spinnaker_SDK_ROOT}/opt/spinnaker NO_DEFAULT_PATH)
+else()
+  message("No Spinnaker_SDK_ROOT defined. Using default locations.")
+  set(_SPINNAKER_SEARCH_ROOT /opt/spinnaker /usr /usr/local)
+endif()
+message("Searching for Spinnaker libraries and includes in: ${_SPINNAKER_SEARCH_ROOT}")
 
-find_library(Spinnaker_LIBRARIES NAMES Spinnaker
-  PATHS
-  /usr/lib
-  /usr/local/lib
-  /opt/spinnaker/lib
-)
+find_path(Spinnaker_INCLUDE_DIR NAMES Spinnaker.h PATHS ${_SPINNAKER_SEARCH_ROOT} PATH_SUFFIXES include include/spinnaker)
+find_library(Spinnaker_LIBRARIES NAMES Spinnaker PATHS ${_SPINNAKER_SEARCH_ROOT} PATH_SUFFIXES lib)
 
-if (Spinnaker_INCLUDE_DIRS AND Spinnaker_LIBRARIES)
+if(Spinnaker_INCLUDE_DIR AND Spinnaker_LIBRARIES)
   set(Spinnaker_FOUND 1)
-endif (Spinnaker_INCLUDE_DIRS AND Spinnaker_LIBRARIES)
+endif()
